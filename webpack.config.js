4e1941c75f8363c4a2a23c40272dedc1f6eb7380
Zelislav <zeljobeska@gmail.com>
2020-12-17 11:37:18 +0100

    Installing HtmlWebpackPlugin & Cache Busting

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "./src/index.js",
     path: path.resolve(__dirname, "dist") // resolving absolute path
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html"
+    })
+  ],
   module: {
     rules: [
       {
