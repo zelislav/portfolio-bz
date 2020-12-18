ab882421d36451485a3a0e02a3307067e705f18d
Zelislav <zeljobeska@gmail.com>
2020-12-18 09:59:55 +0100

    Sticky header

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-unused-vars */
 import MobileMenu from './app/modules/MobileMenu';
 import RevealOnScroll from './app/modules/RevealOnScroll';
+import StickyHeader from './app/modules/StickyHeader';
 import './scss/styles.scss';
 
 const mobileMenu = new MobileMenu();
 new RevealOnScroll(document.querySelectorAll('.feature-item'), '85%');
 new RevealOnScroll(document.querySelectorAll('.experience'), '60%');
+const stickyHeader = new StickyHeader();
