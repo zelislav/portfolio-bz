d072ee242fde6ea0089e702ab5b9f40b19b75e7a
Zelislav <zeljobeska@gmail.com>
2020-12-18 15:37:47 +0100

    Modal lightbox

---
+++ src/app/modules/Modal.js
@@ -0,0 +1,35 @@
+class Modal {
+  constructor () {
+    this.openModalButton = document.querySelectorAll('.open-modal');
+    this.modal = document.querySelector('.modal');
+    this.closeModalButton = document.querySelector('.modal__close');
+    this.events();
+  }
+
+  events () {
+    for (let i = 0; i < this.openModalButton.length; i++) {
+      this.openModalButton[i].addEventListener('click', this.openModal.bind(this));
+    }
+
+    this.closeModalButton.addEventListener('click', this.closeModal.bind(this));
+
+    document.addEventListener('keyup', this.keyPressHandler.bind(this));
+  }
+
+  keyPressHandler (e) {
+    if (e.key === 'Escape') {
+      this.closeModal();
+    }
+  }
+
+  openModal () {
+    this.modal.classList.add('modal--is-visible');
+    return false
+  }
+
+  closeModal () {
+    this.modal.classList.remove('modal--is-visible');
+  }
+}
+
+export default Modal;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@
 import MobileMenu from './app/modules/MobileMenu';
 import RevealOnScroll from './app/modules/RevealOnScroll';
 import StickyHeader from './app/modules/StickyHeader';
+import Modal from './app/modules/Modal'
 import './scss/styles.scss';
 
 const mobileMenu = new MobileMenu();
 new RevealOnScroll(document.querySelectorAll('.feature-item'), '85%');
 new RevealOnScroll(document.querySelectorAll('.experience'), '60%');
 const stickyHeader = new StickyHeader();
+const modal = new Modal();
