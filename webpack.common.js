d0068785f9f141ed742a7ef17d58320e9fcb5d18
Zelislav <zeljobeska@gmail.com>
2021-01-04 16:24:18 +0100

    Lazy loading images

--- webpack.common.js
+++ webpack.common.js
@@ -7,13 +7,31 @@ const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
 module.exports = {
   entry: {
     main: './src/index.js',
-    // vendor: './src/vendor.js'
+    vendor: './src/vendor.js'
   },
   module: {
     rules: [
       {
         test: /\.html$/,
-        use: ['html-loader']
+        use: {
+          loader: 'html-loader',
+          options: {
+            attributes: {
+              list: [
+                {
+                  tag: 'img',
+                  attribute: 'data-src',
+                  type: 'src',
+                },
+                {
+                  tag: 'img',
+                  attribute: 'data-srcset',
+                  type: 'srcset',
+                },
+              ]
+            }
+          }
+        }
       },
       {
         test: /\.(eot|woff|woff2|ttf|png|jpg|gif)$/, // any of them will match
